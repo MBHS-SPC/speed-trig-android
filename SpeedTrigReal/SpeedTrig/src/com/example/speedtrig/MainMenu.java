8b0bcd9f90456c20cb29b6bb9a037e7a7341cc08
eyob-- <tse.eyob@gmail.com>
2014-11-21 15:38:31 -0500

    Revert "New package name."
    This reverts commit 53d11b8949a48886222a735c683f860b0e940c8b.

---
+++ SpeedTrigReal/SpeedTrig/src/com/example/speedtrig/MainMenu.java
@@ -0,0 +1,75 @@
+package com.example.speedtrig;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainMenu extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void startRegular(View v){
+		//Toast.makeText(this, "Ready!", Toast.LENGTH_SHORT).show();
+		//Toast.makeText(this, "Set!", Toast.LENGTH_SHORT).show();
+		//Toast.makeText(this, "Go!", Toast.LENGTH_SHORT).show();
+		RegularTrig.entranceButtonClicked = true;
+		startActivity(new Intent(this, RegularTrig.class));
+	}
+	
+	public void startInverse(View v){
+		Toast.makeText(this, "Coming Soon!!!", Toast.LENGTH_SHORT).show();
+		//startActivity(new Intent(this, InverseTrig.class));
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main_menu,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
