5b556b646169238214376942abc966b658878cb0
Ali Anwar <alia7477@gmail.com>
2015-03-01 14:14:27 -0500

    Implemented Part of NavigationDrawer
    Implemented part of the NavigationDrawer, but currently the app doesn't
    work. I suspect that there's some other way to add
    android.support.v7.appcompat which I'm missing.

--- SpeedTrigReal/SpeedTrig/src/com/example/speedtrig/About.java
+++ SpeedTrigReal/SpeedTrig/src/com/example/speedtrig/About.java
@@ -1,18 +1,29 @@
 package com.example.speedtrig;
 
 import android.app.ListActivity;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
 
 /**
  * Created by AliAnwar7477 on 2/26/2015.
  */
-public class About extends ListActivity {
+public class About extends BaseActivity {
+
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
+
+        navMenuIcons = getResources()
+                .obtainTypedArray(R.array.nav_drawer_icons);//load icons from strings.xml
+
+        set(navMenuTitles,navMenuIcons);
     }
 
     public void destroyActivity(View v){
