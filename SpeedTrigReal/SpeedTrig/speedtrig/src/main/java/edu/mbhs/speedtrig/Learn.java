83afa320cfe321b93191c186ff0eac76369b5af4
suyhmedu <suyhmed@gmail.com>
2015-03-15 23:36:36 -0400

    Learn Section
    Added a section for learning.

---
+++ SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/Learn.java
@@ -0,0 +1,31 @@
+package edu.mbhs.speedtrig;
+
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import com.example.speedtrig.R;
+
+/**
+ * Created by suyhmed on 3/15/15.
+ */
+public class Learn extends BaseActivity {
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
+
+        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);// load icons from
+        // strings.xml
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3f51b5")));
+
+        set(navMenuTitles, navMenuIcons);
+    }
+
