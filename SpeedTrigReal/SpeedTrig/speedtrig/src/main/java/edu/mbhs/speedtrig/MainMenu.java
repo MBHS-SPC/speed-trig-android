0839d74e4e99bd510da6320938d0c633237080a5
Ali Anwar <alia7477@gmail.com>
2015-03-20 20:12:32 -0400

    Revert "Changed Sounds"
    This reverts commit c55f0ca0f7a8b5b7818e8458b04ce7ef3114e9a4.

--- SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/MainMenu.java
+++ SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/MainMenu.java
@@ -97,7 +97,8 @@ public class MainMenu extends BaseActivity /**implements
 
         speedTrigMainTheme = MediaPlayer.create(this, R.raw.speed_trig_main_theme);
         speedTrigMainTheme.setLooping(true);
-        //speedTrigMainTheme.start();
+        //speedTrigMainTheme.setVolume((float)); //Giving an error while runing the program
+        speedTrigMainTheme.start();
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#303F9F")));
 
