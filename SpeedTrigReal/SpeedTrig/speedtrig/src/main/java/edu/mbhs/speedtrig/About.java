a06b118d8204de197e0bc1e631f9dfd5e19717a9
suyhmedu <suyhmed@gmail.com>
2015-03-26 16:56:25 -0400

    Social Media Buttons +More Cards
    Arranged the cards a bit and added up to 17 cards on the Learn Section.
    Added our Social Media Site Icons that are clickable.

--- SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/About.java
+++ SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/About.java
@@ -1,9 +1,14 @@
 package edu.mbhs.speedtrig;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.speedtrig.R;
 
@@ -21,6 +26,33 @@ public class About extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        /*
+         *Start of Social Media Buttons
+         *Give the users clickable Social Media buttons that direct them to
+         * our social media sites such as Facebook and Google+
+         */
+        ImageView googlePlusButton = (ImageView) findViewById(R.id.imageButton);
+        ImageView faceBookButton = (ImageView) findViewById(R.id.imageButton2);
+
+        googlePlusButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent googlePlus = new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/b/112188567527171945812/112188567527171945812/posts"));
+                startActivity(googlePlus);
+            }
+        });
+
+        faceBookButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent faceBook = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/groups/mhbs.spc"));
+                startActivity(faceBook);
+            }
+        });
+//End of Social Media Buttons
 
         navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
 
