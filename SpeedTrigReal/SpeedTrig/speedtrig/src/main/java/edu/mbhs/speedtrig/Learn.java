55d40b099acebde7b5e0d90bdd3ad922204c6c72
suyhmedu <suyhmed@gmail.com>
2015-04-17 23:13:05 -0400

    Link to Online Speed Trig Quiz Generator
    Added an image at the bottom on the Learn section that will take the
    user to the Online Speed Trig Quiz Generator.

--- SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/Learn.java
+++ SpeedTrigReal/SpeedTrig/speedtrig/src/main/java/edu/mbhs/speedtrig/Learn.java
@@ -1,9 +1,13 @@
 package edu.mbhs.speedtrig;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.speedtrig.R;
 
@@ -20,6 +24,17 @@ public class Learn extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
+
+        ImageView TrigGenOnline = (ImageView) findViewById(R.id.imageView28);
+
+        TrigGenOnline.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent TrigGenOnline = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mbhs.edu/~ansarma/rose/"));
+                startActivity(TrigGenOnline);
+            }
+        });
 
         navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
 
