cb04a4f841e3303637c5a09a57bb1ad4ae55d228
eyob-- <tse.eyob@gmail.com>
2015-11-01 11:43:57 -0500

    Added angle choosers
    Added buttons to choose an angle. Selected color = CYAN.

--- speedtrig/src/main/java/edu/mbhs/speedtrig/UnitCircle.java
+++ speedtrig/src/main/java/edu/mbhs/speedtrig/UnitCircle.java
@@ -74,17 +74,59 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
         canvas.drawColor(Color.WHITE);
         Paint paint = new Paint();
 
+
         // Draw unit circle outline
+
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(5);
         // we want the diameter to be 3/4 of the smaller of the two dimensions
-        float bigRadius = Math.min(canvas.getWidth(), canvas.getHeight()) * 3 / 8;
+        final float bigRadius = Math.min(canvas.getWidth(), canvas.getHeight()) * 3 / 8;
         float cx = canvas.getWidth() / 2, cy = canvas.getHeight() / 2;
         canvas.drawCircle(cx, cy, bigRadius, paint);
         // line radius is 7/8 of the smaller of the two dimensions
         float lineRadius = bigRadius * 7 / 6;
         canvas.drawLine(cx - lineRadius, cy, cx + lineRadius, cy, paint);
         canvas.drawLine(cx, cy - lineRadius, cx, cy + lineRadius, paint);
+
+
+
+        // Draw tiny circle "buttons"
+
+        // first angle (default) will already be "selected"
+        paint.setColor(Color.CYAN);
+        paint.setStyle(Paint.Style.FILL);
+        final float buttonRadius = bigRadius / 15;
+        float buttonX = 0, buttonY = 0;
+        // pi/6, pi/3, pi/2, 2pi/3, 5pi/6, pi, 7pi/6, 4pi/3, 3pi/2, 5pi/3, 11pi/6
+        for (float theta = 0; Math.abs(theta - 2 * Math.PI) > 0.001; theta += Math.PI / 6) {
+            Log.d(TAG, "theta: "+theta);
+            buttonX = cx + (float) Math.cos(theta) * bigRadius;
+            buttonY = cy + (float) Math.sin(theta) * bigRadius;
+            // draw filled in circle first
+            canvas.drawCircle(buttonX, buttonY, buttonRadius-2.5f, paint);
+            // then draw outline circle
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(buttonX, buttonY, buttonRadius, paint);
+            // and change the paint back for the next filled in circle
+            paint.setColor(Color.WHITE);
+            paint.setStyle(Paint.Style.FILL);
+        }
+        // pi/4, 3pi/4, 5pi/4, 7pi/4
+        for (float theta = (float)(Math.PI / 4); theta < 2 * Math.PI; theta += Math.PI / 2) {
+            Log.d(TAG, "theta: "+theta);
+            buttonX = cx + (float) Math.cos(theta) * bigRadius;
+            buttonY = cy + (float) Math.sin(theta) * bigRadius;
+            // draw filled in circle first
+            canvas.drawCircle(buttonX, buttonY, buttonRadius-2.5f, paint);
+            // then draw outline circle
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(buttonX, buttonY, buttonRadius, paint);
+            // and change the paint back for the next filled in circle
+            paint.setColor(Color.WHITE);
+            paint.setStyle(Paint.Style.FILL);
+        }
     }
 }
