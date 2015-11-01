f5e7e6529cdaf60eb2a83dd749f72f3e7ef81298
eyob-- <tse.eyob@gmail.com>
2015-11-01 12:24:54 -0500

    Added touch-to-angle detection
    Added method UnitCircle.angleClosestTo() to check which angle the user's touch was closest to

--- speedtrig/src/main/java/edu/mbhs/speedtrig/UnitCircle.java
+++ speedtrig/src/main/java/edu/mbhs/speedtrig/UnitCircle.java
@@ -19,6 +19,7 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
     public static final String TAG = "Interactive Unit Circle";
 
     SurfaceView surfaceView;
+    float cx, cy, bigRadius, buttonRadius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +34,8 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-                    float x = event.getX();
-                    float y = event.getY();
-                    Toast.makeText(UnitCircle.this, x + " " + y, Toast.LENGTH_SHORT).show();
+                    double theta = angleClosestTo(event.getX(), event.getY());
+                    Toast.makeText(UnitCircle.this, theta+"", Toast.LENGTH_SHORT).show();
                 }
                 return true;
             }
@@ -81,8 +81,8 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(5);
         // we want the diameter to be 3/4 of the smaller of the two dimensions
-        final float bigRadius = Math.min(canvas.getWidth(), canvas.getHeight()) * 3 / 8;
-        float cx = canvas.getWidth() / 2, cy = canvas.getHeight() / 2;
+        bigRadius = Math.min(canvas.getWidth(), canvas.getHeight()) * 3 / 8;
+        cx = canvas.getWidth() / 2; cy = canvas.getHeight() / 2;
         canvas.drawCircle(cx, cy, bigRadius, paint);
         // line radius is 7/8 of the smaller of the two dimensions
         float lineRadius = bigRadius * 7 / 6;
@@ -96,13 +96,14 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
         // first angle (default) will already be "selected"
         paint.setColor(Color.CYAN);
         paint.setStyle(Paint.Style.FILL);
-        final float buttonRadius = bigRadius / 15;
+        buttonRadius = bigRadius / 15;
         float buttonX = 0, buttonY = 0;
+
         // pi/6, pi/3, pi/2, 2pi/3, 5pi/6, pi, 7pi/6, 4pi/3, 3pi/2, 5pi/3, 11pi/6
         for (float theta = 0; Math.abs(theta - 2 * Math.PI) > 0.001; theta += Math.PI / 6) {
             Log.d(TAG, "theta: "+theta);
             buttonX = cx + (float) Math.cos(theta) * bigRadius;
-            buttonY = cy + (float) Math.sin(theta) * bigRadius;
+            buttonY = cy - (float) Math.sin(theta) * bigRadius;
             // draw filled in circle first
             canvas.drawCircle(buttonX, buttonY, buttonRadius-2.5f, paint);
             // then draw outline circle
@@ -113,11 +114,11 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
             paint.setColor(Color.WHITE);
             paint.setStyle(Paint.Style.FILL);
         }
+
         // pi/4, 3pi/4, 5pi/4, 7pi/4
         for (float theta = (float)(Math.PI / 4); theta < 2 * Math.PI; theta += Math.PI / 2) {
-            Log.d(TAG, "theta: "+theta);
             buttonX = cx + (float) Math.cos(theta) * bigRadius;
-            buttonY = cy + (float) Math.sin(theta) * bigRadius;
+            buttonY = cy - (float) Math.sin(theta) * bigRadius;
             // draw filled in circle first
             canvas.drawCircle(buttonX, buttonY, buttonRadius-2.5f, paint);
             // then draw outline circle
@@ -128,5 +129,44 @@ public class UnitCircle extends Activity implements SurfaceHolder.Callback {
             paint.setColor(Color.WHITE);
             paint.setStyle(Paint.Style.FILL);
         }
+    }
+
+    /**
+     * Return the angle on the unit circle closest to the given point, or -1 if the point is
+     * farther than 2 * (the button radius) from any valid angle on the unit circle
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return The closest valid angle [0, 2pi) on the drawn unit circle or -1
+     */
+    private double angleClosestTo(float x, float y) {
+        // if the distance is 50 or more, they probably just touched the screen without wanting
+        // to change the angle
+        double minDist = buttonRadius * 2;
+        double closestAngle = -1;
+        double circleX, circleY, dist;
+
+        // pi/6, pi/3, pi/2, 2pi/3, 5pi/6, pi, 7pi/6, 4pi/3, 3pi/2, 5pi/3, 11pi/6
+        for (double theta = 0; Math.abs(theta - 2 * Math.PI) > 0.001; theta += Math.PI / 6) {
+            circleX = cx + Math.cos(theta)*bigRadius;
+            circleY = cy - Math.sin(theta)*bigRadius;
+            dist = Math.hypot(circleX - x, circleY - y);
+            if (dist < minDist) {
+                minDist = dist;
+                closestAngle = theta;
+            }
+        }
+
+        // pi/4, 3pi/4, 5pi/4, 7pi/4
+        for (float theta = (float)(Math.PI / 4); theta < 2 * Math.PI; theta += Math.PI / 2) {
+            circleX = cx + Math.cos(theta)*bigRadius;
+            circleY = cy - Math.sin(theta)*bigRadius;
+            dist = Math.hypot(circleX - x, circleY - y);
+            if (dist < minDist) {
+                minDist = dist;
+                closestAngle = theta;
+            }
+        }
+
+        return closestAngle;
     }
 }
