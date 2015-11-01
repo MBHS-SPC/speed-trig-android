2ab67a7e277f875da201172434ad95ae3de9af63
eyob-- <tse.eyob@gmail.com>
2015-11-01 00:26:41 -0400

    Add Unit Circle Layout Design
    Created Interactive Unit Circle activity and arranged it's layout

---
+++ speedtrig/src/main/java/edu/mbhs/speedtrig/UnitCircle.java
@@ -0,0 +1,15 @@
+package edu.mbhs.speedtrig;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.speedtrig.R;
+
+public class UnitCircle extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_circle);
+    }
+}
